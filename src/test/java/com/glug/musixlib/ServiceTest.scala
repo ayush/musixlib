e4a41c7c31476bf70d8f0f45b47ca3334314949b
Ayush Gupta <ayush@glugbot.com>
2011-05-23 16:48:42 +0530

    better logging

--- src/test/java/com/glug/musixlib/ServiceTest.scala
+++ src/test/java/com/glug/musixlib/ServiceTest.scala
@@ -60,7 +60,7 @@ class ServiceTest extends FunSuite {
     assert(serviceResponse!= null && album != null, "No album retrieved")
     assert(serviceResponse!= null && album.album_name.equals("Play"), "Expected album Play, got something else")
     assert(serviceResponse!= null && album.artist_name.equals("Moby"), "Expected album artist Moby, got something else")
-    println(album)
+//    println(album)
   }
 
 
@@ -75,14 +75,14 @@ class ServiceTest extends FunSuite {
 
     assert(serviceResponse.message.body.track != null, "Unable to fetch track")
     assert(serviceResponse.message.body.track.track_id.equals("673092") && serviceResponse.message.body.track.track_mbid.equals("1e3668b5-3053-40f7-8bda-0ba987b37cbe"), "track id did not match the expected one")
-    println(serviceResponse.message.body.track)
+//    println(serviceResponse.message.body.track)
   }
 
   test("is able to get track Charts") {
     val serviceResponse = service.getTrackCharts(new GetTrackChart("US"))
 
     assert(serviceResponse.message.body.track_list.size() > 0, "Unable to fetch track charts")
-    println(serviceResponse.message.body.track_list.iterator().next().track)
+//    println(serviceResponse.message.body.track_list.iterator().next().track)
   }
 
   test("is able to search for kaleidoscopic and find Porcelain") {
@@ -95,7 +95,7 @@ class ServiceTest extends FunSuite {
     assert(serviceResponse.message.body.track_list.size() > 0, "Unable to search for Porcelain")
     val firstTrack = serviceResponse.message.body.track_list.iterator().next().track
     assert(firstTrack.track_id.equals("673092") && firstTrack.track_mbid.equals("1e3668b5-3053-40f7-8bda-0ba987b37cbe"), "track id did not match the expected one")
-    println(firstTrack)
+//    println(firstTrack)
   }
 
   test("is able to get tracks for album Play") {
@@ -104,7 +104,7 @@ class ServiceTest extends FunSuite {
     assert(serviceResponse.message.body.track_list.size() > 0, "Unable to get tracks for album Play")
     val firstTrack = serviceResponse.message.body.track_list.iterator().next().track
     assert(firstTrack.track_id.equals("673090") && firstTrack.track_mbid.equals("1618b28c-2de2-428a-a460-fbe451b5fbde"), "track id did not match the expected one")
-    println(firstTrack)
+//    println(firstTrack)
   }
 
   test("is able to get track by id") {
